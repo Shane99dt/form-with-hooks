55ce5cd87135339dac094f28cc4eea8a91aca05b
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-01 16:05:48 +0200

    form, using hooks, just changed to multiple useStates

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,74 +3,99 @@ import SuccessPage from "./SuccessPage"
 
 const Form = () => {
 
-  const [formData, setFormData] = useState({
-    firstName : "",
-    lastName : "",
-    email : "",
-    password : "",
-    rememberMe : false,
-    emailIsValid : false,
-    passwordIsValid:false,
-    isSubmitted:false
-  })
+  // const [formData, setFormData] = useState({
+  //   firstName : "",
+  //   lastName : "",
+  //   email : "",
+  //   password : "",
+  //   rememberMe : false,
+  //   emailIsValid : false,
+  //   passwordIsValid:false,
+  //   isSubmitted:false
+  // })
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [rememberMe, setRememberMe] = useState(false)
+  const [emailIsValid, setEmailIsValid] = useState(false)
+  const [passwordIsValid, setPasswordIsValid] = useState(false)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
 
   const handleEmailChange = (e) => {
     const emailInput = e.target.value
     const regex = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
     const result = regex.test(emailInput)
 
-    setFormData({...formData, email : emailInput, emailIsValid: result})
+    // setFormData({...formData, email : emailInput, emailIsValid: result})
+    setEmail(emailInput)
+    setEmailIsValid(result)
   }
 
   const handlePasswordChange = (e) => {
     const passwordInput = e.target.value
     const isValid = passwordInput.length > 5
 
-    setFormData({...formData, password : passwordInput, passwordIsValid: isValid})
+    // setFormData({...formData, password : passwordInput, passwordIsValid: isValid})
+    setPassword(passwordInput)
+    setPasswordIsValid(isValid)
   }
 
   const handleRememberMe = (e) => {
-    setFormData({...formData, rememberMe : e.target.checked})
+    // setFormData({...formData, rememberMe : e.target.checked})
+    setRememberMe(e.target.checked)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if(formData.emailIsValid && formData.passwordIsValid){
-      setFormData({...formData, isSubmitted: true})
+    if(emailIsValid && passwordIsValid){
+      // setFormData({...formData, isSubmitted: true})
+      setIsSubmitted(true)
     }else{
-      setFormData({...formData, isSubmitted: false})
+      // setFormData({...formData, isSubmitted: false})
+      setIsSubmitted(false)
       alert("Fill the fields correctly")
     }
-    console.log(formData)
   }
 
   const handleReset = () => {
-    setFormData({...formData,
-      email : "",
-      password : "",
-      rememberMe : false,
-      emailIsValid : false,
-      passwordIsValid:false,
-      isSubmitted:false
-    })
+    // setFormData({...formData,
+    //   email : "",
+    //   password : "",
+    //   rememberMe : false,
+    //   emailIsValid : false,
+    //   passwordIsValid:false,
+    //   isSubmitted:false
+    // })
+    setEmail("")
+    setEmailIsValid(false)
+    setPassword("")
+    setPasswordIsValid(false)
+    setRememberMe(false)
+    setIsSubmitted(false)
   }
 
   const handleChangeFirstName = (e) => {
-    setFormData({...formData, firstName : e.target.value})
+    // setFormData({...formData, firstName : e.target.value})
+    setFirstName(e.target.value)
   }
 
   const handleChangeLastName = (e) => {
-    setFormData({...formData, lastName : e.target.value})
+    // setFormData({...formData, lastName : e.target.value})
+    setLastName(e.target.value)
   }
 
-  const validEmail = `form-control ${formData.emailIsValid ? 'is-valid' : 'is-invalid'}`
-  const validPassword = `form-control ${formData.passwordIsValid ? 'is-valid' : 'is-invalid'}`
+  // const validEmail = `form-control ${formData.emailIsValid ? 'is-valid' : 'is-invalid'}`
+  // const validPassword = `form-control ${formData.passwordIsValid ? 'is-valid' : 'is-invalid'}`
+  const validEmail = `form-control ${emailIsValid ? 'is-valid' : 'is-invalid'}`
+  const validPassword = `form-control ${passwordIsValid ? 'is-valid' : 'is-invalid'}`
 
 
   return(
     <>
-      {!formData.isSubmitted ? (
+      {!isSubmitted ? (
         <section className="d-flex justify-content-center mt-5">
           <form className="col-5" onSubmit={handleSubmit}>
 
@@ -109,9 +134,9 @@ const Form = () => {
       ) : (
         <>
           <SuccessPage
-            firstName = {formData.firstName}
-            lastName = {formData.lastName}
-            email = {formData.email}
+            firstName = {firstName}
+            lastName = {lastName}
+            email = {email}
             onClick = {handleReset}
           />
         </>
