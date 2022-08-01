8ee4968882b99b766dd8ca08ede060c04a001dce
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-01 14:02:23 +0200

    form, using hooks

--- src/components/SuccessPage.js
+++ src/components/SuccessPage.js
@@ -2,7 +2,7 @@ const SuccessPage = (props) => {
   return(
     <>
       <section className="d-flex align-items-center mt-5 flex-column">
-        <p>Hello <span className="font-weight-bold">{props.firstName}</span> {props.lastName}, </p>
+        <p>Hello <span className="font-weight-bold text-capitalize">{props.firstName}</span> {props.lastName}, </p>
         <p>You have successfully submitted the form using <span className="font-weight-bold font-italic">{props.email}</span></p>
         <button onClick={props.onClick} className="btn btn-primary col-4">Resubmit Form</button>
       </section>
